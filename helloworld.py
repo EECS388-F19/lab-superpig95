9bcdd4c77dbfc9a457475ba38914460b3fc0b196
shuyw <shuyw@umich.edu>
2019-09-09 14:11:45 -0400

    adding helloworld.py

---
+++ helloworld.py
@@ -0,0 +1,14 @@
+import random
+print("Shuyuan Wang")
+
+x = random.randint(0, 100)
+y = random.randint(0, 100)
+print(x)
+print(y)
+
+summ = x+y
+print "Sum =",summ
+print "Average =", float(summ)/2
+
+
+
